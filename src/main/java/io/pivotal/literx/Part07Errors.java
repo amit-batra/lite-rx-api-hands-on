84124f51db4e78beb7f6ab2a15afb5240144fb13
Amit Batra <abatra@adobe.com>
2024-09-08 20:07:20 +0530

    Solved Part07Errors

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -16,6 +16,7 @@
 
 package io.pivotal.literx;
 
+import ch.qos.logback.classic.spi.IThrowableProxy;
 import io.pivotal.literx.domain.User;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
@@ -33,14 +34,14 @@ public class Part07Errors {
 
 	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorReturn(User.SAUL);
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
 	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
+		return flux.onErrorResume(error -> Flux.just(User.SAUL, User.JESSE));
 	}
 
 //========================================================================================
@@ -48,14 +49,25 @@ public class Part07Errors {
 	// TODO Implement a method that capitalizes each user of the incoming flux using the
 	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> {
+			try {
+				return this.capitalizeUser(user);
+			}
+			catch (GetOutOfHereException e) {
+				throw Exceptions.propagate(e);
+			}
+		});
 	}
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
 		if (user.equals(User.SAUL)) {
 			throw new GetOutOfHereException();
 		}
-		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
+		return new User(
+			user.getUsername().toUpperCase(),
+			user.getFirstname().toUpperCase(),
+			user.getLastname().toUpperCase()
+		);
 	}
 
 	protected final class GetOutOfHereException extends Exception {
